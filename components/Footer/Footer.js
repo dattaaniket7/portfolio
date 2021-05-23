bb08567883135943f23b4907a8bd71fbf10a82bb
AniketDCR7 <52018449+AniketDCR7@users.noreply.github.com>
2021-05-23 18:33:40 +0530

    Landing Started

---
+++ components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import s from './Footer.module.scss';
+
+const Footer = () => {
+	return (
+		<footer className={s.footer}>
+			Made with 💗 by <a href="https://www.aniketdatta.rocks/">Aniket Datta</a>
+			<br />
+			I'll see you in the Dark Side of the Moon 🌈
+		</footer>
+	);
+};
+
+export default Footer;
