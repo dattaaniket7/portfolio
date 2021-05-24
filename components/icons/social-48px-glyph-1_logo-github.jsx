0fffecc02ad84fa6cd779d2dc258bedfefe80139
AniketDCR7 <52018449+AniketDCR7@users.noreply.github.com>
2021-05-24 11:43:34 +0530

    Homepage Done, Navbar to be added

---
+++ components/icons/social-48px-glyph-1_logo-github.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Social48pxGlyph1_logoGithub(props) {
+	const fill = props.fill || 'currentColor';
+	const secondaryfill = props.secondaryfill || fill;
+	const strokewidth = props.strokewidth || 1;
+	const width = props.width || '100%';
+	const height = props.height || '100%';
+
+	return (
+		<svg height={height} width={width} viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+	<g fill={fill}>
+		<path d="M24 .6C10.7.6 0 11.3 0 24.6c0 10.6 6.9 19.6 16.4 22.8 1.2.2 1.6-.5 1.6-1.2v-4.1c-6.7 1.5-8.1-3.2-8.1-3.2-1.1-2.8-2.7-3.5-2.7-3.5-2.2-1.5.2-1.5.2-1.5 2.4.2 3.7 2.5 3.7 2.5 2.1 3.7 5.6 2.6 7 2 .2-1.6.8-2.6 1.5-3.2-5.3-.6-10.9-2.7-10.9-11.9 0-2.6.9-4.8 2.5-6.4-.2-.6-1.1-3 .2-6.4 0 0 2-.6 6.6 2.5 1.9-.5 4-.8 6-.8s4.1.3 6 .8c4.6-3.1 6.6-2.5 6.6-2.5 1.3 3.3.5 5.7.2 6.4 1.5 1.7 2.5 3.8 2.5 6.4 0 9.2-5.6 11.2-11 11.8.9.7 1.6 2.2 1.6 4.4v6.6c0 .6.4 1.4 1.7 1.2C41.1 44.2 48 35.2 48 24.6c0-13.3-10.7-24-24-24z" fill={fill} fillRule="evenodd"/>
+	</g>
+</svg>
+	);
+};
+
+export default Social48pxGlyph1_logoGithub;
---
+++ components/icons/social-48px-glyph-1_logo-instagram.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Social48pxGlyph1_logoInstagram(props) {
+	const fill = props.fill || 'currentColor';
+	const secondaryfill = props.secondaryfill || fill;
+	const strokewidth = props.strokewidth || 1;
+	const width = props.width || '100%';
+	const height = props.height || '100%';
+
+	return (
+		<svg height={height} width={width} viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
+	<g fill={fill}>
+		<path d="M42.3 0H5.7C2.6 0 0 2.5 0 5.7v36.6C0 45.5 2.6 48 5.7 48h36.6c3.2 0 5.7-2.5 5.7-5.7V5.7C48 2.5 45.4 0 42.3 0zM36 6h4c1.1 0 2 .9 2 2v4c0 1.1-.9 2-2 2h-4c-1.1 0-2-.9-2-2V8c0-1.1.9-2 2-2zm-11.9 8.8c5.1 0 9.2 4.1 9.2 9.2s-4.1 9.2-9.2 9.2-9.2-4.1-9.2-9.2c-.1-5 4.1-9.2 9.2-9.2zM40 42H8c-1.1 0-2-.9-2-2V20h4c-.6 2.2-.7 4.8-.2 7.2 1.2 5.6 5.7 10 11.3 11.1C30.4 40.1 38.6 33 38.6 24c0-1.3-.3-2.8-.7-4.1h4v20c.1 1.2-.8 2.1-1.9 2.1z" fill={fill}/>
+	</g>
+</svg>
+	);
+};
+
+export default Social48pxGlyph1_logoInstagram;
